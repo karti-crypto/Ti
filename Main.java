5039e637e938dabd1c8f15384c97fe9f3a62cd8f
karti-crypto <tatitc983@gmail.com>
2025-10-05 16:38:46 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner (System.in);
+        String user;
+        
+        do{
+            System.out.println("Ingrese cualquier palabra (Si quieres el SUBETE PAOLA!!, digita: loba):");
+            user=sc.nextLine();
+            if (!user.equals("loba")){
+                System.out.println("Intente nuevamente frutilupis!");
+            }
+       }while (!user.equals("loba"));
+       System.out.println("Es la basuraaaaa!!!");
+       System.out.println("Paola súbete ahí");
+       System.out.println("SúBETE PAOLA SúBETE");
+       System.out.println("Loba");
+       System.out.println("Loba");
+       System.out.println("LOBA");
+       System.out.println("loBA loba");
+       System.out.println("jakja");
+       System.out.println("LOBA PATAS LOBA");
+       System.out.println("loba jakjakaja");
+       System.out.println("JAJSKAJKAJKA");
+       System.out.println("bye brocolí");
+}
+}
